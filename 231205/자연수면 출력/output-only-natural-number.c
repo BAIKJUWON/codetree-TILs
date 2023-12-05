1290ead268ef916f9eea003337997cea6c85c47a
CE_BAIKJUWON <75535083+BAIKJUWON@users.noreply.github.com>
2023-12-05 13:35:02 +0900

    Title: 자연수면 출력, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231205/자연수면 출력/output-only-natural-number.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int i,a,b;
+
+    scanf("%d %d",&a,&b);
+
+    if(a>0){
+        for(i=0;i<b;i++){
+            printf("%d",a);
+        }
+    }
+    return 0;
+}
